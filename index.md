91e4bb0cbd04a20bcaf2cdf481cc1d9c68aa6c4b
Graham Meulendyks <gmeul001@odu.edu>
2022-08-15 10:48:12 -0400

    Update for gh-pages specifics

---
+++ index.md
@@ -0,0 +1,6 @@
+# Project Reports
+
+Website Analyzer
+
+* [Tests](./reports/tests/test/)
+* [JavaDoc](./docs/javadoc/)
--- src/main/resources/scripts/webanalysis.sh
+++
@@ -1,2 +0,0 @@
-# call the jar file with $@, which carries all the arguements from the user
-java -jar webAnalysis.jar $@
